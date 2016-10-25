47c1c4f68b29e47835f0592aa6ab33a8754bfb38
lenni <lenni31@neuf.fr>
2016-10-25 16:24:16 +0200

    commit du 16:24

--- js/app.js
+++ js/app.js
@@ -1,24 +1,44 @@
+// (function(){
+// 	"use strict";
+// 	var app = {
+// 		init:function(){
+// 		$.get('http://192.168.1.40:1337/alice.md', function(data){
+//         var converter = new showdown.Converter();
+//         var html = converter.makeHtml(data);
+//         $('#md').html(html);
+// 		$.get('http://192.168.1.40:1337/menu.json', function(data2){
+// 			$('#app').html('<a>' + data2.menu[1].title + '</a>');
+// 			$('#app').html('<a>' + this.data.menu[0].title + '</a>');
+// 		},
+// 	},
+// 	};
+
+// 	$(document).ready(function(){
+// 		app.init();
+// 	});
+// })();
+
 (function(){
-	"use strict";
-	var app = {
-		init:function(){
-		$.get('http://192.168.1.40:1337/alice.md', function(data){
+ "use strict";
+ 	var app = {
+ 		init:function(){
+ 		$.get('http://192.168.1.40:1337/alice.md', function(data){
         var converter = new showdown.Converter();
         var html = converter.makeHtml(data);
         $('#md').html(html);
-        app.recup();
+ 		});
+ 		$.get('http://192.168.1.40:1337/menu.json', function(data2){
+        $('#app').html('<a href='+test+'>' + data2.menu[0].title + '</a>' +'<br>'+'<br>' + '<a id="mark">' + data2.menu[1].title + '</a>');
+ 		var test = $.get('http://192.168.1.40:1337'+data2.menu[0].path.responseText);
+ 		console.log(test);
+  
+
+
         });
-		},
-
-		recup: function(){
-		$.get('http://192.168.1.40:1337/menu.json', function(data2){
-			$('#app').html(data2.menu[1].title);
-           console.log(data2.menu[1]);
-		});
-		},
-	};
-
-	$(document).ready(function(){
-		app.init();
-	});
-})();
+ 		},
+ 	};
+ 
+ 	$(document).ready(function(){
+ 		app.init();
+ 	});
+ })(); 
